6600f5b0210f0a6e7d069acc03e5eff7295c4f69
ccastanom <159740202+ccastanom@users.noreply.github.com>
2024-02-13 00:58:07 +0000

    :sparkles: class 12-02

---
+++ Examples/Car.java
@@ -0,0 +1,9 @@
+public class Car extends Vehicle {
+    public Car (){
+        System.out.println("Constructor Car");
+        ruedas = 4;
+    }
+    public void pitar(){
+        System.out.println("piiiiii piiii ruedas " + ruedas);
+    }
+}
---
+++ Examples/Vehicle.java
@@ -0,0 +1,10 @@
+public class Vehicle { 
+    protected int ruedas;
+    public Vehicle (){
+        System.out.println("constructor de un vehiculo");
+        ruedas = 0;
+    }
+
+    public int numRuedas() { return ruedas;}
+
+}
