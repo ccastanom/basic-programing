180e534fa3addf396bc9818e99f7afdd20e60470
ccastanom <159740202+ccastanom@users.noreply.github.com>
2024-02-19 19:15:24 -0500

    Delete Examples directory

--- Examples/Car.java
+++
@@ -1,9 +0,0 @@
-public class Car extends Vehicle {
-    public Car (){
-        System.out.println("Constructor Car");
-        ruedas = 4;
-    }
-    public void pitar(){
-        System.out.println("piiiiii piiii ruedas " + ruedas);
-    }
-}
--- Examples/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main (String[] args){  
-        System.out.println("desde el main");
-        Car c = new Car();
-        c.pitar();
-    }
-}
